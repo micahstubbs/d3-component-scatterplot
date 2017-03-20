266d92a674ced40aad19b20a85b63180800cadb7
Micah Stubbs <micah.stubbs@gmail.com>
2017-03-20 05:04:09 -0700

    abstract out spinner

--- 04/vis.js
+++ 04/vis.js
@@ -1,26 +1,4 @@
-/* global d3 Redux wheel */
-
-// This component with a local timer makes the wheel spin.
-const spinner = ((() => {
-  const timer = d3.local();
-  return d3.component('g')
-    .create(function (d) {
-      timer.set(this, d3.timer((elapsed) => {
-        d3.select(this).call(wheel, elapsed * d.speed);
-      }));
-    })
-    .render(function (d) {
-      d3.select(this).attr('transform', `translate(${d.x},${d.y})`);
-    })
-    .destroy(function (d) {
-      timer.get(this).stop();
-      return d3.select(this)
-        .attr('fill-opacity', 1)
-        .transition().duration(3000)
-          .attr('transform', `translate(${d.x},${d.y}) scale(10)`)
-          .attr('fill-opacity', 0);
-    });
-})());
+/* global d3 Redux wheel spinner */
 
 const axis = ((() => {
   const axisLocal = d3.local();

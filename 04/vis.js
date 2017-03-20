23abac2bdcbf04a2c7f2663b9ee75c60e01a6106
Micah Stubbs <micah.stubbs@gmail.com>
2017-03-20 05:27:27 -0700

    abstract out reducer

--- 04/vis.js
+++ 04/vis.js
@@ -1,36 +1,4 @@
-/* global d3 Redux wheel spinner axis scatterplot tooltip app loadData */
-
-function reducer(state, action) {
-  state = state || {
-    width: 960,
-    height: 500 - 38,
-    loading: true,
-    margin: { top: 12, right: 12, bottom: 40, left: 50 },
-    x: 'acceleration',
-    y: 'horsepower',
-    color: 'cylinders',
-    radius: 'weight',
-  };
-  switch (action.type) {
-    case 'INGEST_DATA':
-      return Object.assign({}, state, {
-        loading: false,
-        data: action.data,
-        numericColumns: action.numericColumns,
-        ordinalColumns: action.ordinalColumns,
-      });
-    case 'SET_X':
-      return Object.assign({}, state, { x: action.column });
-    case 'SET_Y':
-      return Object.assign({}, state, { y: action.column });
-    case 'SET_COLOR':
-      return Object.assign({}, state, { color: action.column });
-    case 'SET_RADIUS':
-      return Object.assign({}, state, { radius: action.column });
-    default:
-      return state;
-  }
-}
+/* global d3 Redux wheel spinner axis scatterplot tooltip app loadData reducer */
 
 function actionsFromDispatch(dispatch) {
   return {

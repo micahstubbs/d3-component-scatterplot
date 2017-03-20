d98d4fcb016e484746d49cafb4652b2f375edd1c
Micah Stubbs <micah.stubbs@gmail.com>
2017-03-20 05:23:31 -0700

    abstract out loadData

--- 04/vis.js
+++ 04/vis.js
@@ -1,35 +1,4 @@
-/* global d3 Redux wheel spinner axis scatterplot tooltip app */
-
-function loadData(actions) {
-  const numericColumns = [
-    'acceleration',
-    'cylinders',
-    'displacement',
-    'horsepower',
-    'weight',
-    'year',
-    'mpg',
-  ];
-
-  const ordinalColumns = [
-    'cylinders',
-    'origin',
-    'year',
-  ];
-
-  setTimeout(() => { // Show off the spinner for a few seconds ;)
-    d3.csv('auto-mpg.csv', type, (data) => {
-      actions.ingestData(data, numericColumns, ordinalColumns);
-    });
-  }, 2000);
-
-  function type(d) {
-    return numericColumns.reduce((d, column) => {
-      d[column] = +d[column];
-      return d;
-    }, d);
-  }
-}
+/* global d3 Redux wheel spinner axis scatterplot tooltip app loadData */
 
 function reducer(state, action) {
   state = state || {

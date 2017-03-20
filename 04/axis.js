aea6fbadd8dd299db5676b9bbe55aa3b02d53e98
Micah Stubbs <micah.stubbs@gmail.com>
2017-03-20 05:06:23 -0700

    abstract out axis

---
+++ 04/axis.js
@@ -0,0 +1,25 @@
+/* global d3 */
+
+const axis = ((() => {
+  const axisLocal = d3.local();
+  return d3.component('g')
+    .create(function (d) {
+      axisLocal.set(this, d3[`axis${d.type}`]());
+      d3.select(this)
+        .attr('opacity', 0)
+        .call(axisLocal.get(this)
+          .scale(d.scale)
+          .ticks(d.ticks || 10))
+          .transition('opacity').duration(2000)
+            .attr('opacity', 0.8);
+    })
+    .render(function (d) {
+      d3.select(this)
+        .attr('transform', `translate(${[
+          d.translateX || 0,
+          d.translateY || 0,
+        ]})`)
+        .transition('ticks').duration(3000)
+          .call(axisLocal.get(this));
+    });
+})());
--- 04/vis.js
+++ 04/vis.js
@@ -1,28 +1,4 @@
-/* global d3 Redux wheel spinner */
-
-const axis = ((() => {
-  const axisLocal = d3.local();
-  return d3.component('g')
-    .create(function (d) {
-      axisLocal.set(this, d3[`axis${d.type}`]());
-      d3.select(this)
-        .attr('opacity', 0)
-        .call(axisLocal.get(this)
-          .scale(d.scale)
-          .ticks(d.ticks || 10))
-          .transition('opacity').duration(2000)
-            .attr('opacity', 0.8);
-    })
-    .render(function (d) {
-      d3.select(this)
-        .attr('transform', `translate(${[
-          d.translateX || 0,
-          d.translateY || 0,
-        ]})`)
-        .transition('ticks').duration(3000)
-          .call(axisLocal.get(this));
-    });
-})());
+/* global d3 Redux wheel spinner axis */
 
 // This component displays the visualization.
 const scatterPlot = ((() => {
